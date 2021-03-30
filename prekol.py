735ffdeed1ad3a1e441582bbcc96da3f379c0799
George Joev <80751077+vurdeez@users.noreply.github.com>
2021-03-30 10:57:18 +0300

    Update prekol.py

--- prekol.py
+++ prekol.py
@@ -1,1 +1,10 @@
-python3 -m venv myvenv
+from matplotlib import pyplot
+import random
+x = range(0,25)
+y = [random.randint(0,100) for r in range(0,25)]
+fig1 = pyplot.figure()
+pyplot.plot(x, y, '-')
+pyplot.title('First Plot - Random integers')
+pyplot.xlabel('X Axis')
+pyplot.ylabel('Y Axis')
+pyplot.show()
